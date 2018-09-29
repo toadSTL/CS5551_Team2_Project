fbe42135c23383a83541e0a848c467a0c6c267d2
toadSTL <gregory.dylan.brown@gmail.com>
2018-09-28 22:46:24 -0500

    Increment 1

---
+++ Increment 1/source/testUMKCParkingApp/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HttpClientModule } from '@angular/common/http'; 
+
+
+import { LoginPage } from '../pages/login/login';
+import { SignUpPage } from '../pages/signup/signup';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    LoginPage,
+    SignUpPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    HttpClientModule,
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    LoginPage,
+    SignUpPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}

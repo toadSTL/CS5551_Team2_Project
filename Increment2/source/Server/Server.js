63dc0ee039736c844098be6a78aae073c3025df8
toadSTL <gregory.dylan.brown@gmail.com>
2018-10-26 20:35:09 -0500

    server

---
+++ Increment2/source/Server/Server.js
@@ -0,0 +1,24 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var bodyParser = require('body-parser');
+var express = require('express');
+var cors = require('cors');
+
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.get('/posts', function(req, res) {
+
+    console.log("posts!");
+
+});
+
+var server = app.listen(8081,function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at http://%s:%s", host, port)
+});
